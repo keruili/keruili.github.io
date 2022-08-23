f4f5fa2145008fdb29075642c503d32c3d5d0515
keruili <111748130+keruili@users.noreply.github.com>
2022-08-23 16:26:44 +0800

    Add files via upload

--- RUNJEMDOC.bat
+++ RUNJEMDOC.bat
@@ -1,6 +1,6 @@
 @echo off
 echo|set /p="Generating html files from jemdoc files... " 
-jemdoc.py   "home.jemdoc"
+jemdoc.py   "index.jemdoc"
 jemdoc.py   "education.jemdoc"
 jemdoc.py   "Publications.jemdoc"
 jemdoc.py  "awards.jemdoc"
